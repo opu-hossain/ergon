e615d3daa901ad30938888eead529e69f7ef3386
opu <opu.hossain.data@gmail.com>
2026-07-13 00:45:57 +0600

    feat: implimented tagged union Value type with bool/nil support. - Dynamically typed numbers - Unary negation and runtime errors - re-implimented binary arithmetic operators with the new changes - Implimented true, false, and nil - Added falsyness - Added Equality and comparison

--- include/ergon/chunk.h
+++ include/ergon/chunk.h
@@ -6,10 +6,17 @@
 
 typedef enum {
   OP_CONSTANT,
+  OP_EQUAL,
+  OP_GREATER,
+  OP_LESS,
+  OP_NIL,
+  OP_TRUE,
+  OP_FALSE,
   OP_ADD,
   OP_SUBTRACT,
   OP_MULTIPLY,
   OP_DEVIDE,
+  OP_NOT,
   OP_NEGATE,
   OP_RETURN,
 } OpCode;
--- src/vm.c
+++ src/vm.c
@@ -1,3 +1,4 @@
+#include <stdarg.h>
 #include <stdio.h>
 
 #include "../include/ergon/common.h"
@@ -6,10 +7,23 @@
 #include "../include/ergon/memory.h"
 #include "../include/ergon/vm.h"
 #include "ergon/chunk.h"
+#include "ergon/value.h"
 
 VM vm;
 
 static void reset_stack() { vm.stack_top = vm.stack; }
+
+static void runtime_error(const char *format, ...) {
+  va_list args;
+  va_start(args, format);
+  vfprintf(stderr, format, args);
+  va_end(args);
+  fputs("\n", stderr);
+
+  size_t instruction = vm.ip - vm.chunk->code - 1;
+  int line = get_line(vm.chunk, (int)instruction);
+  fprintf(stderr, "[line %d] in script\n]", line);
+}
 
 void init_vm() { reset_stack(); }
 
@@ -37,15 +51,24 @@ Value pop() {
   return *vm.stack_top;
 }
 
+static Value peek(int distance) { return vm.stack_top[-1 - distance]; }
+
+static bool is_falsey(Value value) {
+  return IS_NIL(value) || (IS_BOOL(value) && !AS_BOOL(value));
+}
+
 static interpret_result run() {
 #define READ_BYTE() (*vm.ip++)
 #define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
-#define BINARY_OP(op)                                                          \
+#define BINARY_OP(value_type, op)                                              \
   do {                                                                         \
-    double b = *(vm.stack_top - 1);                                            \
-    double a = *(vm.stack_top - 2);                                            \
-    vm.stack_top--;                                                            \
-    *(vm.stack_top - 1) = a op b;                                              \
+    if (!IS_NUMBER(peek(0)) || !IS_NUMBER(peek(1))) {                          \
+      runtime_error("Operands must be nubmers.");                              \
+      return INTERPRET_RUNTIME_ERROR;                                          \
+    }                                                                          \
+    double b = AS_NUMBER(pop());                                               \
+    double a = AS_NUMBER(pop());                                               \
+    push(value_type(a op b));                                                  \
   } while (false)
   for (;;) {
 #ifdef DEBUG_TRACE_EXECUTION
@@ -64,21 +87,48 @@ static interpret_result run() {
       Value constant = READ_CONSTANT();
       push(constant);
       break;
+    case OP_EQUAL: {
+      Value a = pop();
+      Value b = pop();
+      push(BOOL_VAL(values_equal(a, b)));
+      break;
+    }
+    case OP_GREATER:
+      BINARY_OP(BOOL_VAL, >);
+      break;
+    case OP_LESS:
+      BINARY_OP(BOOL_VAL, <);
+      break;
+    case OP_NIL:
+      push(NIL_VAL);
+      break;
+    case OP_TRUE:
+      push(BOOL_VAL(true));
+      break;
+    case OP_FALSE:
+      push(BOOL_VAL(false));
+      break;
     case OP_ADD:
-      BINARY_OP(+);
+      BINARY_OP(NUMBER_VAL, +);
       break;
     case OP_SUBTRACT:
-      BINARY_OP(-);
+      BINARY_OP(NUMBER_VAL, -);
       break;
     case OP_MULTIPLY:
-      BINARY_OP(*);
+      BINARY_OP(NUMBER_VAL, *);
       break;
     case OP_DEVIDE:
-      BINARY_OP(/);
+      BINARY_OP(NUMBER_VAL, /);
       break;
-
+    case OP_NOT:
+      push(BOOL_VAL(is_falsey(pop())));
+      break;
     case OP_NEGATE:
-      *(vm.stack_top - 1) = -*(vm.stack_top - 1);
+      if (!IS_NUMBER(peek(0))) {
+        runtime_error("Operand must be a number.");
+        return INTERPRET_RUNTIME_ERROR;
+      }
+      push(NUMBER_VAL(-AS_NUMBER(pop())));
       break;
     case OP_RETURN:
       print_value(pop());
